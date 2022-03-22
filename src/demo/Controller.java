4454955295da60ed531877561ebdfd13db781fa7
M1ster Anonymous <zhringz@icloud.com>
2022-03-22 20:03:36 +0800

    0322

--- src/demo/Controller.java
+++ src/demo/Controller.java
@@ -1,9 +1,8 @@
 package demo;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
@@ -12,7 +11,7 @@ import java.io.File;
 
 public class Controller {
     private Stage stage;
-    public FileChooser fileChooser;
+    public FileChooser fileChooser = new FileChooser();
     private Wrapper wrapper;
     @FXML
     public AnchorPane ap;
@@ -91,24 +90,32 @@ public class Controller {
         switch (op) {
             case "SGD":
                 opText2.setText("momentum:");
+                opParam2.setText("0");
                 opText3.setText("dampening:");
+                opParam3.setText("0");
                 opText4.setText("weight_decay:");
                 opText4.setVisible(true);
+                opParam4.setText("0");
                 opParam4.setVisible(true);
                 opText5.setText("nesterov:");
                 opText5.setVisible(true);
+                opParam5.setText("False");
                 opParam5.setVisible(true);
                 opText6.setVisible(false);
                 opParam6.setVisible(false);
                 break;
             case "ASGD":
                 opText2.setText("lambd:");
+                opParam2.setText("0.0001");
                 opText3.setText("alpha:");
+                opParam3.setText("0.75");
                 opText4.setText("t0:");
                 opText4.setVisible(true);
+                opParam4.setText("1000000.0");
                 opParam4.setVisible(true);
                 opText5.setText("weight_decay:");
                 opText5.setVisible(true);
+                opParam5.setText("0");
                 opParam5.setVisible(true);
                 opText6.setVisible(false);
                 opParam6.setVisible(false);
@@ -116,9 +123,12 @@ public class Controller {
             case "Adam":
             case "Adamax":
                 opText2.setText("betas:");
+                opParam2.setText("(0.9, 0.999)");
                 opText3.setText("eps:");
+                opParam3.setText("0.00000001");
                 opText4.setText("weight_decay:");
                 opText4.setVisible(true);
+                opParam4.setText("0");
                 opParam4.setVisible(true);
                 opText5.setVisible(false);
                 opParam5.setVisible(false);
@@ -127,9 +137,12 @@ public class Controller {
                 break;
             case "Adadelta":
                 opText2.setText("rho:");
+                opParam2.setText("0.9");
                 opText3.setText("eps:");
+                opParam3.setText("0.000001");
                 opText4.setText("weight_decay:");
                 opText4.setVisible(true);
+                opParam4.setText("0");
                 opParam4.setVisible(true);
                 opText5.setVisible(false);
                 opParam5.setVisible(false);
@@ -138,7 +151,9 @@ public class Controller {
                 break;
             case "Adagrad":
                 opText2.setText("lr_decay:");
+                opParam2.setText("0");
                 opText3.setText("weight_decay:");
+                opParam3.setText("0");
                 opText4.setVisible(false);
                 opParam4.setVisible(false);
                 opText5.setVisible(false);
@@ -148,7 +163,9 @@ public class Controller {
                 break;
             case "Rprop":
                 opText2.setText("etas:");
+                opParam2.setText("(0.5, 1.2)");
                 opText3.setText("step_sizes:");
+                opParam3.setText("(0.000001, 50)");
                 opText4.setVisible(false);
                 opParam4.setVisible(false);
                 opText5.setVisible(false);
@@ -158,15 +175,20 @@ public class Controller {
                 break;
             case "RMSprop":
                 opText2.setText("alpha:");
+                opParam2.setText("0.99");
                 opText3.setText("eps:");
+                opParam3.setText("0.00000001");
                 opText4.setText("weight_decay:");
                 opText4.setVisible(true);
+                opParam4.setText("0");
                 opParam4.setVisible(true);
                 opText5.setText("momentum:");
                 opText5.setVisible(true);
+                opParam5.setText("0");
                 opParam5.setVisible(true);
                 opText6.setText("centered:");
                 opText6.setVisible(true);
+                opParam6.setText("False");
                 opParam6.setVisible(true);
                 break;
         }
@@ -185,71 +207,83 @@ public class Controller {
     @FXML
     public void selectLoss() {
         String loss = losscb.getValue();
+        lossPane.setVisible(true);
         switch (loss) {
             case "BCELoss":
-                lossPane.setVisible(true);
                 lossText1.setText("weight:");
                 lossText1.setVisible(true);
+                lossParam1.setText("None");
                 lossParam1.setVisible(true);
                 lossText2.setText("size_average:");
                 lossText2.setVisible(true);
+                lossParam2.setText("True");
                 lossParam2.setVisible(true);
                 lossText3.setText("reduce:");
                 lossText3.setVisible(true);
+                lossParam3.setText("True");
                 lossParam3.setVisible(true);
                 lossText4.setText("reduction:");
                 lossText4.setVisible(true);
+                lossParam4.setText("'mean'");
+                lossParam4.setVisible(true);
+                lossText5.setVisible(false);
+                lossParam5.setVisible(false);
+                break;
+            case "NLLLoss":
+                lossText1.setText("weight:");
+                lossText1.setVisible(true);
+                lossParam1.setText("None");
+                lossParam1.setVisible(true);
+                lossText2.setText("size_average:");
+                lossText2.setVisible(true);
+                lossParam2.setText("None");
+                lossParam2.setVisible(true);
+                lossText3.setText("reduce:");
+                lossText3.setVisible(true);
+                lossParam3.setText("None");
+                lossParam3.setVisible(true);
+                lossText4.setText("reduction:");
+                lossText4.setVisible(true);
+                lossParam4.setText("'mean'");
                 lossParam4.setVisible(true);
                 lossText5.setVisible(false);
                 lossParam5.setVisible(false);
                 break;
             case "BCEWithLogitsLoss":
-                lossPane.setVisible(true);
                 lossText1.setText("weight:");
                 lossText1.setVisible(true);
+                lossParam1.setText("None");
                 lossParam1.setVisible(true);
                 lossText2.setText("size_average:");
                 lossText2.setVisible(true);
+                lossParam2.setText("True");
                 lossParam2.setVisible(true);
                 lossText3.setText("reduce:");
                 lossText3.setVisible(true);
+                lossParam3.setText("True");
                 lossParam3.setVisible(true);
                 lossText4.setText("reduction:");
                 lossText4.setVisible(true);
+                lossParam4.setText("'mean'");
                 lossParam4.setVisible(true);
                 lossText5.setText("pos_weight:");
                 lossText5.setVisible(true);
+                lossParam5.setText("None");
                 lossParam5.setVisible(true);
                 break;
-            case "NLLLoss":
-                lossPane.setVisible(true);
-                lossText1.setText("weight:");
-                lossText1.setVisible(true);
-                lossParam1.setVisible(true);
-                lossText2.setText("size_average:");
-                lossText2.setVisible(true);
-                lossParam2.setVisible(true);
-                lossText3.setText("ignore_index:");
-                lossText3.setVisible(true);
-                lossParam3.setVisible(true);
-                lossText4.setText("reduce:");
-                lossText4.setVisible(true);
-                lossParam4.setVisible(true);
-                lossText5.setText("reduction:");
-                lossText5.setVisible(true);
-                lossParam5.setVisible(true);
-                break;
-            case "SmoothL1Loss":
             case "L1Loss":
-                lossPane.setVisible(true);
+            case "MSELoss":
                 lossText1.setText("size_average:");
                 lossText1.setVisible(true);
+                lossParam1.setText("True");
                 lossParam1.setVisible(true);
                 lossText2.setText("reduce:");
                 lossText2.setVisible(true);
+                lossParam2.setText("True");
                 lossParam2.setVisible(true);
                 lossText3.setText("reduction:");
                 lossText3.setVisible(true);
+                lossParam3.setText("'mean'");
                 lossParam3.setVisible(true);
                 lossText4.setVisible(false);
                 lossParam4.setVisible(false);
@@ -257,33 +291,99 @@ public class Controller {
                 lossParam5.setVisible(false);
                 break;
             case "CrossEntropyLoss":
-            case "MSELoss":
-                lossPane.setVisible(false);
+                lossText1.setText("weight:");
+                lossText1.setVisible(true);
+                lossParam1.setText("None");
+                lossParam1.setVisible(true);
+                lossText2.setText("size_average:");
+                lossText2.setVisible(true);
+                lossParam2.setText("True");
+                lossParam2.setVisible(true);
+                lossText3.setVisible(false);
+                lossParam3.setVisible(false);
+                lossText4.setVisible(false);
+                lossParam4.setVisible(false);
+                lossText5.setVisible(false);
+                lossParam5.setVisible(false);
+                break;
+            case "SmoothL1Loss":
+                lossText1.setText("size_average:");
+                lossText1.setVisible(true);
+                lossParam1.setText("True");
+                lossParam1.setVisible(true);
+                lossText2.setText("reduce:");
+                lossText2.setVisible(true);
+                lossParam2.setText("True");
+                lossParam2.setVisible(true);
+                lossText3.setText("reduction:");
+                lossText3.setVisible(true);
+                lossParam3.setText("'mean'");
+                lossParam3.setVisible(true);
+                lossText4.setText("beta:");
+                lossText4.setVisible(true);
+                lossParam4.setText("1.0");
+                lossParam4.setVisible(true);
+                lossText5.setVisible(false);
+                lossParam5.setVisible(false);
                 break;
         }
         wrapper.setLossFunc(loss);
         levelPane.setVisible(true);
     }
-//torch.nn.BCELoss(weight=None, size_average=None, reduce=None, reduction='mean')
-//torch.nn.BCEWithLogitsLoss(weight=None, size_average=None, reduce=None, reduction='mean', pos_weight=None)
-//torch.nn.NLLLoss(weight=None, size_average=None, ignore_index=-100, reduce=None, reduction='mean')
-//torch.nn.CrossEntropyLoss()
-//torch.nn.L1Loss(size_average=None, reduce=None, reduction='mean')
-//torch.nn.MSELoss()
-//torch.nn.SmoothL1Loss(size_average=None, reduce=None, reduction='mean')
+//torch.nn.BCELoss(weight=None, size_average=True, reduce=True, reduction='mean')
+//torch.nn.BCEWithLogitsLoss(weight=None, size_average=True, reduce=True, reduction='mean', pos_weight=None)
+//torch.nn.NLLLoss(weight=None, size_average=None, reduce=None, reduction='mean')
+//torch.nn.CrossEntropyLoss(weight=None, size_average=True)
+//torch.nn.L1Loss(size_average=True, reduce=True, reduction='mean')
+//torch.nn.MSELoss(size_average=True, reduce=True, reduction='mean')
+//torch.nn.SmoothL1Loss(size_average=True, reduce=True, reduction='mean', beta=1.0)
 
 
     @FXML
     public void saveAll() {
-        // TODO: save all settings from textfields into wrapper
+        Alert saveAlert = new Alert(
+                Alert.AlertType.CONFIRMATION,
+                "Save all settings?",
+                ButtonType.YES,
+                ButtonType.NO,
+                ButtonType.CANCEL
+        );
+        saveAlert.setTitle("Confirm");
+        saveAlert.showAndWait();
+        if (saveAlert.getResult() == ButtonType.YES) {
+            //TODO: save all settings from textfields into wrapper
+            disableAll();
+            generate.setDisable(false);
+        }
+    }
 
-        generate.setDisable(false);
+    public void disableAll() {
+        readbt.setDisable(true);
+        netcb.setDisable(true);
+        activatorcb.setDisable(true);
+        opcb.setDisable(true);
+        losscb.setDisable(true);
+        opPane.setDisable(true);
+        lossPane.setDisable(true);
+        levelPane.setDisable(true);
     }
 
     @FXML
     public void generateCode() {
         // TODO: check if model file exists
         //  alert and generate model
+        Alert genAlert = new Alert(
+                Alert.AlertType.CONFIRMATION,
+                "Continue to generate model?",
+                ButtonType.YES,
+                ButtonType.NO,
+                ButtonType.CANCEL
+        );
+        genAlert.setTitle("Confirm");
+        genAlert.showAndWait();
+        if (genAlert.getResult() == ButtonType.YES) {
+//            Platform.exit();
+        }
     }
 
     public void init(Stage primaryStage) {
